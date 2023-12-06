3c5cbd92c88c87baf027efcb08f54a0ed05d0fc6
MdN Data <nd.mirica@gmail.com>
2023-12-06 11:41:05 +0200

    updated with map and array of books

--- src/assets/components/BookList.jsx
+++ src/assets/components/BookList.jsx
@@ -1,42 +1,13 @@
 import React from "react";
 import Book from "./Book";
+import { books } from "./data";
 
 const BookList = () => {
   return (
     <section>
-      <Book />
-      <Book
-        src="./imgs/snoop-doog-cook-book.jpg"
-        text="From Crook to Cook : Platinum Recipes"
-        author="Snoop Dogg"
-      />
-      <Book
-        src="https://images-na.ssl-images-amazon.com/images/I/71hwUY5ZmxL._AC_UL600_SR600,400_.jpg"
-        text="Interest Facts For Curious Minds"
-        author="Jordan moore"
-      />
-      <Book
-        src="./imgs/snoop-doog-cook-book.jpg"
-        text="From Crook to Cook : Platinum Recipes"
-        author="Snoop Dogg"
-      />
-      <Book />
-      <Book
-        src="https://images-na.ssl-images-amazon.com/images/I/71hwUY5ZmxL._AC_UL600_SR600,400_.jpg"
-        text="Interest Facts For Curious Minds"
-        author="Jordan moore"
-      />
-      <Book />
-      <Book
-        src="https://images-na.ssl-images-amazon.com/images/I/71hwUY5ZmxL._AC_UL600_SR600,400_.jpg"
-        text="Interest Facts For Curious Minds"
-        author="Jordan moore"
-      />
-      <Book
-        src="./imgs/snoop-doog-cook-book.jpg"
-        text="From Crook to Cook : Platinum Recipes"
-        author="Snoop Dogg"
-      />
+      {books.map((book) => {
+        return <Book src={book.src} text={book.text} author={book.author} />;
+      })}
     </section>
   );
 };
---
+++ src/assets/components/data.js
@@ -0,0 +1,22 @@
+export const books = [
+  {
+    src: "./imgs/snoop-doog-cook-book.jpg",
+    text: "From Crook to Cook : Platinum Recipes",
+    author: "Snoop Dogg",
+  },
+  {
+    src: "https://images-na.ssl-images-amazon.com/images/I/71hwUY5ZmxL._AC_UL600_SR600,400_.jpg",
+    text: "Interest Facts For Curious Minds",
+    author: "Jordan moore",
+  },
+  {
+    src: "https://images-na.ssl-images-amazon.com/images/I/91vnzZO5yPL._AC_UL600_SR600,400_.jpg",
+    text: "The Very Hungry Caterpillar",
+    author: "Eric Carle",
+  },
+  {
+    src: "https://images-na.ssl-images-amazon.com/images/I/91n7p-j5aqL._AC_UL600_SR600,400_.jpg",
+    text: "Fourth Wing (The Empyrean, 1)",
+    author: "Rebecca Yarros",
+  },
+];
